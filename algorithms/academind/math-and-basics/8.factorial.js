d9d56836834ecbf3370e8f45bf0a6072902e729b
leentaylor <leentaylor@gmail.com>
2023-09-15 17:14:47 +1000

    factorial different ways including recursion

---
+++ algorithms/academind/math-and-basics/8.factorial.js
@@ -0,0 +1,48 @@
+function test(actual, expected) {
+  if (expected === actual) {
+    console.log("PASS");
+  } else {
+    console.log(`FAIL: expected: ${expected}, actual: ${actual}`);
+  }
+
+  console.log("");
+}
+
+const factorialWhile = (n) => {
+  if (n === 1) return n;
+  // eg if n is 6 return 6 * 5 * 4 * 3 * 2 * 1;
+  // eg if n is 3 return 3 * 2 * 1;
+  let total = n;
+  let current = n;
+  while (current !== 1) {
+    total *= current - 1;
+    current--;
+  }
+  return total;
+};
+
+const factorialFor = (n) => {
+  let result = n;
+  for (let i = 2; i < n; i++) result *= i;
+  return result;
+};
+
+const factorial = (n, currentTotal) => {
+  // throw on negative numbers
+
+  let subtotal = currentTotal ?? 1;
+
+  if (n === 1) return subtotal;
+
+  return factorial(n - 1, subtotal * n);
+};
+
+test(factorial(6), 720);
+test(factorial(7), 720 * 7);
+test(factorial(3), 6);
+
+// this is my guess from the prompt. time complexity would be O(n) linear time as for each n we loop
+
+// ok so why did I use a while here? maybe because recent examples have been 'while'. I hardly ever use while.
+// lets just do it as a for loop
+// then we can have a go at recursion as that will be the next one
